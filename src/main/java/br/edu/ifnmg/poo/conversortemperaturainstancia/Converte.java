df6d1b3374bd7afa6d2e16dad06876e31e90dff8
analu <analu@DESKTOP-2SF1V6T>
2024-06-24 20:17:18 -0300

    add getters e setters

--- src/main/java/br/edu/ifnmg/poo/conversortemperaturainstancia/Converte.java
+++ src/main/java/br/edu/ifnmg/poo/conversortemperaturainstancia/Converte.java
@@ -5,13 +5,29 @@ package br.edu.ifnmg.poo.conversortemperaturainstancia;
  * @author analu
  */
 public class Converte {
+
     private double valor;
     private String unidade;
 
     public Converte() {
     }
-    
-    
-    
-    
+
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+//</editor-fold>
+
 }
