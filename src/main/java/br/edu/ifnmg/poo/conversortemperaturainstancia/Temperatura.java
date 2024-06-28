8f663de585dddf699a578e1277eff952fabd69ac
AnaLuizanc <analuizanc13@gmail.com>
2024-06-28 16:20:06 -0300

    troca do nome da classe

---
+++ src/main/java/br/edu/ifnmg/poo/conversortemperaturainstancia/Temperatura.java
@@ -0,0 +1,32 @@
+package br.edu.ifnmg.poo.conversortemperaturainstancia;
+
+/**
+ *
+ * @author analu
+ */
+public class Temperatura {
+
+    private double temperatura;
+
+    public Temperatura() {
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+//</editor-fold>
+    public double converteCelsiusParaKelvin() throws TemperaturaInvalidaException {
+        if (temperatura < -273.15) {
+            throw new TemperaturaInvalidaException("Valor da temperatura abaixo do zero absoluto");
+        } else {
+            return temperatura + 273.15;
+        }
+    }
+
+}
