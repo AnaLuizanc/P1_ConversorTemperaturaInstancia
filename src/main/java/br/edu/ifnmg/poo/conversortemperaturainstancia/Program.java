4d0167be687247feee781c187b0129b928414a01
AnaLuizanc <analuizanc13@gmail.com>
2024-06-28 22:50:06 -0300

    add tratamento de exceções e testes

--- src/main/java/br/edu/ifnmg/poo/conversortemperaturainstancia/Program.java
+++ src/main/java/br/edu/ifnmg/poo/conversortemperaturainstancia/Program.java
@@ -9,6 +9,7 @@ public class Program {
     public static void main(String[] args) {
         Temperatura temperatura = new Temperatura();
 
+        //<editor-fold defaultstate="collapsed" desc="Testes de conversão de Celsius para Kelvin">
         try {
             temperatura.setTemperatura(21.0);
             System.out.println(">> Resultado: " + temperatura.converteCelsiusParaKelvin() + " K");
@@ -32,5 +33,33 @@ public class Program {
             System.out.println("ERRO: " + e.getMessage());
         }
 
+//</editor-fold>
+
+//<editor-fold defaultstate="collapsed" desc="Testes de conversão de Celsius para Fahrenheit">
+
+    try {
+            temperatura.setTemperatura(22.0);
+            System.out.println(">> Resultado: " + temperatura.converteCelsiusParaFahrenheit() + " °F");
+        } catch (TemperaturaInvalidaException e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }
+
+        try {
+            temperatura.setTemperatura(-290.0);
+            System.out.println(">> Resultado: " + temperatura.converteCelsiusParaFahrenheit() + " °F");
+
+        } catch (TemperaturaInvalidaException e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }
+
+        try {
+            temperatura.setTemperatura(32.7);
+            System.out.println(">> Resultado: " + temperatura.converteCelsiusParaFahrenheit() + " °F");
+
+        } catch (TemperaturaInvalidaException e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }
+
+//</editor-fold>
     }
 }
